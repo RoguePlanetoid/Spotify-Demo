8f08ca2194294edfd8ca9f018583165e638d7298
RoguePlanetoid <rogueplanetoid@hotmail.com>
2019-05-18 21:01:35 +0100

    Initial Commit

---
+++ Spotify.Demo/Spotify.Demo/Settings.cs
@@ -0,0 +1,8 @@
+﻿namespace Spotify.Demo
+{
+    public static class Settings
+    {
+        public static string ClientId => "<clientid>";
+        public static string ClientSecret => "<clientsecret>";
+    }
+}
